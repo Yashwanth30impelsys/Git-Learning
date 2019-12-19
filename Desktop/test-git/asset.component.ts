9a9fab7592c574e9e232fb6ce27262a49a2928fa
yashwanth <yashwanth.reddy@impelsys.com>
2019-12-19 18:08:01 +0530

    Added asset component

---
+++ Desktop/test-git/asset.component.ts
@@ -0,0 +1,14 @@
+import {component,ngOnInIt} from "@angular/core";
+
+@Component{
+selector:"",
+template:"",
+
+
+export class AssetComponent implements onInIt{
+ngOnInIt(){
+
+function myFunction(){
+return "This is Yashwanth"
+}
+}
